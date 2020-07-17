82090917afbc55a88e6028a372334769c07cec66
J.Y. Yang <1184yang@gmail.com>
2020-07-17 16:53:59 +0800

    Site updated: 2020-07-17 16:53:42

---
+++ 2020/06/12/C-Entity-Framework-Core-2-0-and-PostgreSQL-Provider/demo_ot_drop.sql
@@ -0,0 +1,12 @@
+DROP TABLE order_items;  
+DROP TABLE orders;
+DROP TABLE inventories;
+DROP TABLE products;
+DROP TABLE product_categories;
+DROP TABLE warehouses;
+DROP TABLE employees;
+DROP TABLE contacts;
+DROP TABLE customers;
+DROP TABLE locations;
+DROP TABLE countries;
+DROP TABLE regions;
